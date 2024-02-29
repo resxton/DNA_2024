48d61bcc7ea133d00303d0beb69468191fcfb2da
resxton <gornomirov661@gmail.com>
2024-03-01 00:23:53 +0300

    gitignore

--- LR3/pages/main/index.js
+++ LR3/pages/main/index.js
@@ -1,10 +1,11 @@
+import {ButtonComponent} from "../../components/button/index.js";
 export class MainPage {
     constructor(parent) {
         this.parent = parent;
     }
     
     render() {
-		this.parent.insertAdjacentHTML('beforeend', '<button type="button" class="btn btn-primary">Hello world 3!</button>');
-	}
+        const button = new ButtonComponent(this.parent)
+        button.render()
+    }
 }
-
