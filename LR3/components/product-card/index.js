5c94745600e4d3c9b40e1a84f1d577eb300eaa6c
resxton <gornomirov661@gmail.com>
2024-03-01 10:06:43 +0300

    completed LR3

--- LR3/components/product-card/index.js
+++ LR3/components/product-card/index.js
@@ -3,56 +3,30 @@ export class ProductCardComponent {
         this.parent = parent;
     }
 
-    getHTML() {
-		return (
-			`
-				<div class="card" style="width: 300px;">
-					<img class="card-img-top" src="https://i.pinimg.com/originals/c9/ea/65/c9ea654eb3a7398b1f702c758c1c4206.jpg" alt="картинка">
-					<div class="card-body">
-						<h5 class="card-title">Акция</h5>
-						<p class="card-text">Вот тут информация об акции</p>
-						<button class="btn btn-primary"">Нажми на меня</button>
-					</div>
-				</div>
-			`
-		)
-	}
-	
-	render() {
-		const html = this.getHTML()
-		this.parent.insertAdjacentHTML('beforeend', html)
-	}
-
-	getHTML(data) {
-		return (
-			`
-				<div class="card" style="width: 300px;">
-					<img class="card-img-top" src="${data.src}" alt="картинка">
-					<div class="card-body">
-						<h5 class="card-title">${data.title}</h5>
-						<p class="card-text">${data.text}</p>
-						<button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Нажми на меня</button>
-					</div>
-				</div>
-			`
-		)
-	}
-	
-	render(data) {
-		const html = this.getHTML(data)
-		this.parent.insertAdjacentHTML('beforeend', html)
-	}
-
-	addListeners(data, listener) {
-		document
-			.getElementById(`click-card-${data.id}`)
-			.addEventListener("click", listener)
-	}
-	
-	render(data, listener) {
-		const html = this.getHTML(data)
-		this.parent.insertAdjacentHTML('beforeend', html)
-		this.addListeners(data, listener)
-	}
-
+    getHTML(data) {
+        return (
+            `
+                <div class="card" style="width: 300px;">
+                    <img class="card-img-top" src="${data.src}" alt="картинка">
+                    <div class="card-body">
+                        <h5 class="card-title">${data.title}</h5>
+                        <p class="card-text">${data.text}</p>
+                        <button class="btn btn-primary" id="click-card-${data.id}" data-id="${data.id}">Нажми на меня</button>
+                    </div>
+                </div>
+            `
+        )
+    }
+    
+    addListeners(data, listener) {
+        document
+            .getElementById(`click-card-${data.id}`)
+            .addEventListener("click", listener)
+    }
+    
+    render(data, listener) {
+        const html = this.getHTML(data)
+        this.parent.insertAdjacentHTML('beforeend', html)
+        this.addListeners(data, listener)
+    }
 }
--- LR3/components/product/index.js
+++ LR3/components/product/index.js
@@ -1,30 +1,85 @@
 export class ProductComponent {
     constructor(parent) {
-        this.parent = parent
+        this.parent = parent;
     }
 
     getHTML(data) {
-        return (
-            `
-                <div class="card mb-3" style="width: 540px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="${data.src}" class="img-fluid" alt="картинка">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">${data.title}</h5>
-                                <p class="card-text">${data.text}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            `
-        )
-    }
+		return (
+			`
+				<div class="card mb-3" style="width: 540px;">
+					<div class="row g-0">
+						<div class="col-md-4">
+							<img src="${data.src}" class="img-fluid" alt="картинка">
+						</div>
+						<div class="col-md-8">
+							<div class="card-body">
+								<h5 class="card-title">${data.title}</h5>
+								<p class="card-text">${data.text}</p>
+								<button class="btn btn-primary" id="showAccordionBtn">Купить</button>
+								<div class="accordion mt-3" id="accordionExample" style="display: none;">
+									<div class="accordion-item">
+										<h2 class="accordion-header" id="headingOne">
+											<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+												Стоимость корма
+											</button>
+										</h2>
+										<div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+											<div class="accordion-body">
+												Средняя стоимость корма составляет 3584$ за кг.
+											</div>
+										</div>
+									</div>
+									<div class="accordion-item">
+										<h2 class="accordion-header" id="headingOne">
+											<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+												Стоимость ветеринара
+											</button>
+										</h2>
+										<div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+											<div class="accordion-body">
+												Средняя стоимость похода в ветклинику составляет 9324924$ за 1 раз.
+											</div>
+										</div>
+									</div>
+									<div class="accordion-item">
+										<h2 class="accordion-header" id="headingOne">
+											<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+												Прогулки
+											</button>
+										</h2>
+										<div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+											<div class="accordion-body">
+												Гулять с собакой надо минимум 78 раз в день.
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			`
+		);
+	}
+	
 
     render(data) {
-        const html = this.getHTML(data)
-        this.parent.insertAdjacentHTML('beforeend', html)
+        const html = this.getHTML(data);
+        this.parent.insertAdjacentHTML('beforeend', html);
+
+        const showAccordionBtn = document.getElementById('showAccordionBtn');
+        showAccordionBtn.addEventListener('click', () => {
+            this.showAccordion();
+        });
+    }
+
+    showAccordion() {
+        const accordion = document.getElementById('accordionExample');
+        
+        // Check if the accordion is currently visible
+        const isAccordionVisible = accordion.style.display !== 'none';
+
+        // Toggle the visibility of the accordion
+        accordion.style.display = isAccordionVisible ? 'none' : 'block';
     }
 }
